a6a016ace7f8a3bee93b4cbdc24620dd771cc33b
Raihanul-Ameen <raihanulameenmaheen@gmail.com>
2026-07-07 18:27:15 +0600

    99.5

--- src/main/java/tk/jandev/crystalspeed/mixin/EndcrystalMixin.java
+++ src/main/java/tk/jandev/crystalspeed/mixin/EndcrystalMixin.java
@@ -1,13 +1,11 @@
 package tk.jandev.crystalspeed.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EndCrystalEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.decoration.EndCrystalEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,19 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EndCrystalEntity.class)
 public abstract class EndcrystalMixin {
 
-    @Shadow public abstract void kill();
-    
-    MinecraftClient mc = MinecraftClient.getInstance();
-
     @Inject(at = @At("HEAD"), method = "damage")
-    private void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        MinecraftClient mc = MinecraftClient.getInstance();
         if (mc.player != null) {
             StatusEffectInstance weakness = mc.player.getStatusEffect(StatusEffects.WEAKNESS);
             StatusEffectInstance strength = mc.player.getStatusEffect(StatusEffects.STRENGTH);
             
             if (weakness != null && strength != null) {
                 if (strength.getAmplifier() > weakness.getAmplifier()) {
-                    this.kill();
+                    // Call discard() directly instead of a shadow kill method
+                    ((EndCrystalEntity)(Object)this).discard();
                 }
             }
         }
